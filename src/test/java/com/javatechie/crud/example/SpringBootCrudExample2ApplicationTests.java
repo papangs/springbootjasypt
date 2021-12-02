8e6d6bdc2264d9abdc48fe47766946e89cccc716
LAPTOP-LTAPOCEJ\Irawan Papang S <irawanpapangsubakti28@gmail.com>
2021-12-02 07:11:33 +0700

    upload first

---
+++ src/test/java/com/javatechie/crud/example/SpringBootCrudExample2ApplicationTests.java
@@ -0,0 +1,30 @@
+package com.javatechie.crud.example;
+
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.junit.jupiter.api.Test;
+
+class SpringBootCrudExample2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void testEncryptionKey() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("javatechie"); // encryptor's private key
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+
+        String plaintext = "";
+        System.out.println("Encrypted key : " + encryptor.encrypt(plaintext));
+    }
+
+}
